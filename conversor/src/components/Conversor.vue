649b859f478c9b1c8e5268bfa27cf2f4d8864336
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-11-22 12:21:13 -0300

    BUILT - create the component Conversor

---
+++ conversor/src/components/Conversor.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="conversor">
+    <h2>{{ moedaA }} To {{ moedaB }}</h2>
+    <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
+    <input type="button" value="Convert">
+    <h2>{{ moedaB_value }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConversorUS',
+  props: ['moedaA', 'moedaB'],
+  data() {
+    return {
+      moedaA_value: "",
+      moedaB_value: 0
+    }
+  },
+  methods: {
+
+    converter() {
+
+    }
+  }
+}
+</script>
+
+<style scroped></style>
